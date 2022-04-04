e12bff846c89d4b5191870bf0f8a50e2c4ab02c3
Yusuke Hosonuma <tobi462@gmail.com>
2022-04-04 22:52:14 +0900

    welcome: swiftformat

---
+++ Makefile
@@ -0,0 +1,2 @@
+format:
+	mint run swiftformat .
--- Sources/SwiftUI-Common/SwiftUI_Common.swift
+++ Sources/SwiftUI-Common/SwiftUI_Common.swift
@@ -1,6 +1,5 @@
 public struct SwiftUI_Common {
     public private(set) var text = "Hello, World!"
 
-    public init() {
-    }
+    public init() {}
 }
