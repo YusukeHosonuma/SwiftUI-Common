43301019f92467a6cd57f31d7c703448bf4dbe3b
Yusuke Hosonuma <tobi462@gmail.com>
2022-04-17 14:57:17 +0900

    fix: build error on macOS

--- Example/Shared/Extension/Section+UpperCompatible.swift
+++ Example/Shared/Extension/Section+UpperCompatible.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 @available(iOS, introduced: 13.0, deprecated: 15.0)
+@available(macOS, introduced: 10.15, deprecated: 12.0)
 extension Section where Parent == Text, Content: View, Footer == EmptyView {
     public init<S>(_ title: S, @ViewBuilder content: () -> Content) where S: StringProtocol {
         self.init(content: content) {
