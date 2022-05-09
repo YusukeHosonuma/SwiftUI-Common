c20bb7c5274ab0f18af12ee9a786fdc0b7a6da58
Yusuke Hosonuma <tobi462@gmail.com>
2022-05-09 14:18:12 +0900

    fix: use `canImport`

--- Sources/SwiftUICommon/Extension/Image+.swift
+++ Sources/SwiftUICommon/Extension/Image+.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 public extension Image {
-    #if os(macOS)
+    #if canImport(AppKit)
     init(image: NSImage) {
         self = Image(nsImage: image)
     }
-    #else
+    #endif
+    
+    #if canImport(UIKit)
     init(image: UIImage) {
         self = Image(uiImage: image)
     }
