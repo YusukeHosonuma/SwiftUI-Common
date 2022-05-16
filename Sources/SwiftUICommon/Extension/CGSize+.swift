0b369cfcf86110046e8983741a22fd7e628d2f8d
Yusuke Hosonuma <tobi462@gmail.com>
2022-05-16 10:11:29 +0900

    add: CGSize extensions

--- Sources/SwiftUICommon/Extension/CGSize+.swift
+++ Sources/SwiftUICommon/Extension/CGSize+.swift
@@ -8,7 +8,30 @@
 import SwiftUI
 
 public extension CGSize {
+    /// Alias for `self <= other`
     func contains(_ other: CGSize) -> Bool {
-        width < other.width && height < other.height
+        width <= other.width && height <= other.height
+    }
+}
+
+extension CGSize: Comparable {
+    public static func < (lhs: CGSize, rhs: CGSize) -> Bool {
+        lhs.width < rhs.width && lhs.height < rhs.height
+    }
+}
+
+extension CGSize: AdditiveArithmetic {
+    public static func + (lhs: CGSize, rhs: CGSize) -> CGSize {
+        .init(
+            width: lhs.width + rhs.width,
+            height: lhs.height + rhs.height
+        )
+    }
+
+    public static func - (lhs: CGSize, rhs: CGSize) -> CGSize {
+        .init(
+            width: lhs.width - rhs.width,
+            height: lhs.height - rhs.height
+        )
     }
 }
