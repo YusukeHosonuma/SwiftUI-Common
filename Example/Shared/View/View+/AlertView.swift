6ef6f415ec8cbecee848628ac4b8447bf45415ad
Yusuke Hosonuma <tobi462@gmail.com>
2022-05-24 19:09:27 +0900

    add: alert() to View+

---
+++ Example/Shared/View/View+/AlertView.swift
@@ -0,0 +1,101 @@
+//
+//  AlertView.swift
+//  Example (iOS)
+//
+//  Created by Yusuke Hosonuma on 2022/05/24.
+//
+
+import SwiftUI
+import SwiftUICommon
+
+enum MyError {
+    case warning, fatal
+
+    var helpMessage: String {
+        switch self {
+        case .warning: return "This is warning."
+        case .fatal: return "This is fatal."
+        }
+    }
+}
+
+extension MyError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .warning: return "Warning"
+        case .fatal: return "Fatal"
+        }
+    }
+}
+
+struct AlertView: View {
+    @State var error: MyError? = nil
+    @State var errorWithMessage: MyError? = nil
+
+    var body: some View {
+        VStack(spacing: 8) {
+            VStack {
+                Text("Simple")
+                Button("Warning") { error = .warning }
+                Button("Fatal") { error = .fatal }
+            }
+
+            VStack {
+                Text("With message")
+                Button("Warning") { errorWithMessage = .warning }
+                Button("Fatal") { errorWithMessage = .fatal }
+            }
+        }
+        .extend { content in
+            //
+            // ✅ Not need to specify `isPresented: Binding<Bool>`.
+            //
+            if #available(iOS 15, *) {
+                content
+                    .alert(error: $error) {}
+                    .alert(error: $errorWithMessage) { _ in
+                        Button("OK") {}
+                    } message: { error in
+                        Text(error.helpMessage)
+                    }
+            } else {
+                //
+                // ☑️ Note:
+                // This conditional branching is a workaround for a bug in SwiftUI.
+                // It is not necessary if `.alert` is not used more than once.
+                //
+                if let _ = error {
+                    content
+                        .alert(
+                            error: $error,
+                            dismissButton: .cancel() // Optional
+                        )
+                    // or:
+                    //
+                    // .alert(
+                    //     error: $error,
+                    //     primaryButton: .default(Text("OK")),
+                    //     secondaryButton: .cancel()
+                    // )
+                } else if let _ = errorWithMessage {
+                    content
+                        .alert(
+                            error: $errorWithMessage,
+                            message: Text(errorWithMessage?.helpMessage ?? "unknown"),
+                            dismissButton: .cancel() // Optional
+                        )
+                    // or:
+                    //
+                    // .alert(
+                    //     error: $errorWithMessage,
+                    //     message: Text(errorWithMessage?.helpMessage ?? "unknown"),
+                    //     primaryButton: .default(Text("OK")),
+                    //     secondaryButton: .cancel()
+                    // )
+                } else {
+                    content
+                }
+            }
+        }
+    }
+}
--- Tests/SwiftUICommonTests/Binding+Tests.swift
+++ Tests/SwiftUICommonTests/Binding+Tests.swift
@@ -7,16 +7,34 @@
 
 import XCTest
 import SwiftUI
-@testable import SwiftUICommon
 
 final class BindingExtensionTests: XCTestCase {
-    func testComparable() throws {
+    func testInverted() throws {
         var flag = false
         
         let binding: Binding<Bool> = .init(get: { flag }, set: { flag = $0 })
-        XCTAssertTrue(binding.inverted().wrappedValue)
+        let inverted = binding.inverted()
         
-        binding.inverted().wrappedValue = false
+        XCTAssertTrue(inverted.wrappedValue)
+        
+        inverted.wrappedValue = false
         XCTAssertTrue(flag)
+        XCTAssertFalse(inverted.wrappedValue)
+    }
+
+    func testIsPresented() throws {
+        var value: String? = "Hello"
+        
+        let binding: Binding<String?> = .init(get: { value }, set: { value = $0 })
+        let isPresent = binding.isPresent()
+        
+        XCTAssertTrue(isPresent.wrappedValue)
+        
+        isPresent.wrappedValue = true
+        XCTAssertEqual("Hello", value)
+
+        isPresent.wrappedValue = false
+        XCTAssertNil(value)
+        XCTAssertFalse(isPresent.wrappedValue)
     }
 }
