aad0e0b253e268da461f9faa2e8a4bea88d66478
bamboo-wood <cantate.1009.0417@gmail.com>
2022-06-04 19:06:00 +0900

    Update View+Extension

--- Sources/SwiftUICommon/Extension/View+.swift
+++ Sources/SwiftUICommon/Extension/View+.swift
@@ -104,6 +104,32 @@ public extension View {
             }
         }
     }
+    
+    // MARK: - Layout
+    
+    func hLeading() -> some View {
+        self.frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    func hTrailing() -> some View {
+        self.frame(maxWidth: .infinity, alignment: .trailing)
+    }
+    
+    func hCenter() -> some View {
+        self.frame(maxWidth: .infinity, alignment: .center)
+    }
+    
+    func vTop() -> some View {
+        self.frame(maxHeight: .infinity, alignment: .top)
+    }
+    
+    func vBottom() -> some View {
+        self.frame(maxHeight: .infinity, alignment: .bottom)
+    }
+    
+    func vCenter() -> some View {
+        self.frame(maxHeight: .infinity, alignment: .center)
+    }
 }
 
 @available(iOS 15, *)
